baed39e466f36a39d1705ae6e378caf2f07dc91f
Dyonovan <lookinforfun0000@gmail.com>
2015-02-04 18:05:37 -0500

    Forgot @Mod (Dyo Derp)

--- src/main/java/com/dyonovan/itemreplication/ItemReplication.java
+++ src/main/java/com/dyonovan/itemreplication/ItemReplication.java
@@ -4,6 +4,7 @@ import com.dyonovan.itemreplication.handlers.BlockHandler;
 import com.dyonovan.itemreplication.handlers.ConfigHandler;
 import com.dyonovan.itemreplication.lib.Constants;
 import com.dyonovan.itemreplication.proxy.CommonProxy;
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
@@ -16,6 +17,8 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.config.Configuration;
+
+@Mod(name = Constants.MODNAME, modid = Constants.MODID, version = Constants.VERSION, dependencies = Constants.DEPENDENCIES)
 
 public class ItemReplication {
 
