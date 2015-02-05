532492ef67f55adc96855916f8e25683ca8550da
Dyonovan <lookinforfun0000@gmail.com>
2015-02-04 20:46:45 -0500

    Refactored Ore / Fluid name

---
+++ src/main/java/com/dyonovan/itemreplication/blocks/BlockOreActinium.java
@@ -0,0 +1,25 @@
+package com.dyonovan.itemreplication.blocks;
+
+import com.dyonovan.itemreplication.ItemReplication;
+import com.dyonovan.itemreplication.lib.Constants;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
+public class BlockOreActinium extends Block {
+
+    public BlockOreActinium() {
+        super(Material.rock);
+        this.setBlockName(Constants.MODID + ":BlockOreActinium");
+        this.setCreativeTab(ItemReplication.tabItemReplication);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerBlockIcons(IIconRegister register) {
+        this.blockIcon = register.registerIcon(Constants.MODID + ":actinium");
+    }
+
+}
--- src/main/java/com/dyonovan/itemreplication/handlers/BlockHandler.java
+++ src/main/java/com/dyonovan/itemreplication/handlers/BlockHandler.java
@@ -1,7 +1,7 @@
 package com.dyonovan.itemreplication.handlers;
 
 import com.dyonovan.itemreplication.blocks.BlockFluidActinium;
-import com.dyonovan.itemreplication.blocks.OreActinium;
+import com.dyonovan.itemreplication.blocks.BlockOreActinium;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraftforge.fluids.Fluid;
@@ -21,7 +21,7 @@ public class BlockHandler {
         GameRegistry.registerBlock(blockfluidActinium, "fluidActinium");
         fluidActinium.setUnlocalizedName(blockfluidActinium.getUnlocalizedName());
 
-        blockOreActinium = new OreActinium();
+        blockOreActinium = new BlockOreActinium();
         GameRegistry.registerBlock(blockOreActinium, "oreActinium");
         //blockOreActinium.setBlockTextureName("itemreplication:technetium");
     }
