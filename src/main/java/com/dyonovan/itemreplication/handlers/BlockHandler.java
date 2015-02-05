13e2bb2aec9a9ed02504d7968c7524f444a69e88
Dyonovan <lookinforfun0000@gmail.com>
2015-02-04 19:20:20 -0500

    initial fluid work

--- src/main/java/com/dyonovan/itemreplication/handlers/BlockHandler.java
+++ src/main/java/com/dyonovan/itemreplication/handlers/BlockHandler.java
@@ -1,8 +1,23 @@
 package com.dyonovan.itemreplication.handlers;
 
+import com.dyonovan.itemreplication.blocks.BlockFluidTechnetium;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+
 public class BlockHandler {
 
+    public static Fluid fluidTechnetium;
+    public static Block blockfluidTechnetium;
+
     public static void init() {
+
+        fluidTechnetium = new Fluid("fluidTechnetium");
+        FluidRegistry.registerFluid(fluidTechnetium);
+        blockfluidTechnetium = new BlockFluidTechnetium();
+        GameRegistry.registerBlock(blockfluidTechnetium, "fluidTechnetium");
+        fluidTechnetium.setUnlocalizedName(blockfluidTechnetium.getUnlocalizedName());
 
     }
 }
