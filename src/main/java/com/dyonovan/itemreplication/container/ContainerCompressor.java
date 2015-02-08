abb6efe61d29fb3400d52e938e18ead2c9931ee2
Dyonovan <lookinforfun0000@gmail.com>
2015-02-07 19:44:14 -0500

    Initial Compressor onUpdate

--- src/main/java/com/dyonovan/itemreplication/container/ContainerCompressor.java
+++ src/main/java/com/dyonovan/itemreplication/container/ContainerCompressor.java
@@ -1,19 +1,63 @@
 package com.dyonovan.itemreplication.container;
 
 import com.dyonovan.itemreplication.tileentity.TileCompressor;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ICrafting;
 
 public class ContainerCompressor extends Container {
 
     private TileCompressor tile;
+    private int lastPower, lastTank;
 
     public ContainerCompressor(TileCompressor tile) {
         this.tile = tile;
+        this.lastPower = 0;
+        this.lastTank = 0;
     }
 
     @Override
-    public boolean canInteractWith(EntityPlayer p_75145_1_) {
+    public boolean canInteractWith(EntityPlayer player) {
         return true;
+    }
+
+    @Override
+    public void addCraftingToCrafters(ICrafting crafter) {
+        super.addCraftingToCrafters(crafter);
+        crafter.sendProgressBarUpdate(this, 0, this.tile.getEnergyLevel());
+        crafter.sendProgressBarUpdate(this, 1, this.tile.tank.getFluid().amount);
+    }
+
+    @Override
+    public void detectAndSendChanges() {
+        super.detectAndSendChanges();
+
+        for (int i = 0; i < this.crafters.size(); i++) {
+            ICrafting icrafting = (ICrafting) this.crafters.get(i);
+
+            if (this.lastPower != this.tile.getEnergyLevel())
+                icrafting.sendProgressBarUpdate(this, 0, this.tile.getEnergyLevel());
+            if (this.tile.tank == null)
+                this.tile.tank.setFluid(this.tile.setFluidStack(0));
+            if (this.lastTank != this.tile.tank.getInfo().fluid.amount)
+                icrafting.sendProgressBarUpdate(this, 1, this.tile.tank.getInfo().fluid.amount);
+        }
+
+        this.lastPower = this.tile.getEnergyLevel();
+        this.lastTank = this.tile.tank.getFluid().amount;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void updateProgressBar(int i, int j) {
+        switch (i) {
+            case 0:
+                this.tile.setEnergy(j);
+                break;
+            case 1:
+                this.tile.tank.setFluid(this.tile.setFluidStack(j));
+        }
     }
 }
--- src/main/java/com/dyonovan/itemreplication/tileentity/TileCompressor.java
+++ src/main/java/com/dyonovan/itemreplication/tileentity/TileCompressor.java
@@ -15,29 +15,35 @@ import java.util.List;
 
 public class TileCompressor extends BaseTile implements IFluidHandler, ITeslaHandler {
 
-    public static FluidTank tank = new FluidTank(FluidContainerRegistry.BUCKET_VOLUME);
-    private TeslaBank energyTesla = new TeslaBank(1000);
+    public static FluidTank tank;
+    private TeslaBank energy;
 
     public TileCompressor() {
+        energy = new TeslaBank(1000);
+        tank = new FluidTank(FluidContainerRegistry.BUCKET_VOLUME * 10);
+    }
 
+    public FluidStack setFluidStack(int amount) {
+        FluidStack fluidstack = new FluidStack(BlockHandler.fluidCompressedAir, amount);
+        return fluidstack;
     }
 
     @Override
     public void readFromNBT(NBTTagCompound tag) {
         super.readFromNBT(tag);
-        energyTesla.readFromNBT(tag);
+        energy.readFromNBT(tag);
     }
 
     @Override
     public void writeToNBT(NBTTagCompound tag) {
         super.writeToNBT(tag);
-        energyTesla.writeToNBT(tag);
+        energy.writeToNBT(tag);
     }
 
 
     @Override
     public int fill(ForgeDirection from, FluidStack resource, boolean doFill) {
-        return 0;
+        return tank.fill(resource, doFill);
     }
 
     @Override
@@ -46,14 +52,12 @@ public class TileCompressor extends BaseTile implements IFluidHandler, ITeslaHan
         {
             return null;
         }
-        tank.setFluid(new FluidStack(BlockHandler.fluidCompressedAir, FluidContainerRegistry.BUCKET_VOLUME));
-        return tank.drain(resource.amount, false);
+        return tank.drain(resource.amount, true);
     }
 
     @Override
     public FluidStack drain(ForgeDirection from, int maxDrain, boolean doDrain) {
-        tank.setFluid(new FluidStack(BlockHandler.fluidCompressedAir, FluidContainerRegistry.BUCKET_VOLUME));
-        return tank.drain(maxDrain, false);
+        return tank.drain(maxDrain, true);
     }
 
     @Override
@@ -73,34 +77,45 @@ public class TileCompressor extends BaseTile implements IFluidHandler, ITeslaHan
 
     @Override
     public void addEnergy(int maxAmount) {
-        energyTesla.addEnergy(maxAmount);
+        energy.addEnergy(maxAmount);
     }
 
     @Override
     public void drainEnergy(int maxAmount) {
-        energyTesla.drainEnergy(maxAmount);
+        energy.drainEnergy(maxAmount);
     }
 
     @Override
     public int getEnergyLevel() {
-        return energyTesla.getEnergyLevel();
+        return energy.getEnergyLevel();
     }
 
     @Override
     public TeslaBank getEnergyBank() {
-        return energyTesla;
+        return energy;
+    }
+
+    public void setEnergy(int amount) {
+        energy.setEnergyLevel(amount);
     }
 
     @Override
     public void updateEntity() {
         super.updateEntity();
-        if(energyTesla.canAcceptEnergy()) {
+        if (worldObj.isRemote) return;
+
+        if(energy.canAcceptEnergy()) {
             chargeFromCoils();
+        }
+        if (energy.getEnergyLevel() > 1 && tank.getFluid().amount  < tank.getCapacity()) {
+            energy.drainEnergy(2);
+            tank.fill(setFluidStack(100), true);
+
         }
     }
 
     public void chargeFromCoils() {
-        int maxFill = energyTesla.getMaxCapacity() - energyTesla.getEnergyLevel();
+        int maxFill = energy.getMaxCapacity() - energy.getEnergyLevel();
         List<TileTeslaCoil> coils = findCoils(worldObj, this);
         int currentDrain = 0;
         for(TileTeslaCoil coil : coils) {
@@ -110,11 +125,11 @@ public class TileCompressor extends BaseTile implements IFluidHandler, ITeslaHan
             currentDrain += fill;
             coil.drainEnergy(fill);
 
-            if(worldObj.isRemote)
+            //if(worldObj.isRemote)
                 Minecraft.getMinecraft().effectRenderer.addEffect(new LightningBolt(worldObj, xCoord + 0.5, yCoord + 0.5, zCoord + 0.5, coil.xCoord + 0.5, coil.yCoord + 0.5, coil.zCoord + 0.5, fill > 4 ? fill : 4, new Color(255, 255, 255, 255)));
         }
         while(currentDrain > 0) {
-            energyTesla.addEnergy(ConfigHandler.tickTesla);
+            energy.addEnergy(ConfigHandler.tickTesla);
             currentDrain--;
         }
     }
