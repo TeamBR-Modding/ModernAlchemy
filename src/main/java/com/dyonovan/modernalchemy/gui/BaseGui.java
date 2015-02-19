052def3799ec7a36ab543cd83fd013a5f4ce136a
pauljoda <pauldavis101@gmail.com>
2015-02-18 20:19:06 -0600

    Tool tip zones

--- src/main/java/com/dyonovan/modernalchemy/gui/BaseGui.java
+++ src/main/java/com/dyonovan/modernalchemy/gui/BaseGui.java
@@ -10,10 +10,12 @@ import java.util.List;
 public abstract class BaseGui extends GuiContainer {
 
     protected List<Widget> widgets;
+    protected List<Zone> toolTips;
 
     public BaseGui(Container c) {
         super(c);
         widgets = new ArrayList<Widget>();
+        toolTips = new ArrayList<Zone>();
     }
 
     @Override
@@ -23,8 +25,37 @@ public abstract class BaseGui extends GuiContainer {
         }
     }
 
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float par3) {
+        super.drawScreen(mouseX, mouseY, par3);
+        for(Zone zone : toolTips) {
+            if(zone.isWithin(mouseX, mouseY))
+                renderToolTip(mouseX, mouseY, zone.value);
+        }
+    }
+
     public void renderToolTip(int x, int y, List<String> strings)
     {
         drawHoveringText(strings, x, y, fontRendererObj);
     }
+
+    protected class Zone {
+        int x;
+        int y;
+        int width;
+        int height;
+        List<String> value;
+        protected Zone(int i, int j, int w, int h, List<String> v) {
+            x = i;
+            y = j;
+            width = w;
+            height = h;
+            value = v;
+        }
+
+        public boolean isWithin(int mx, int my) {
+            return mx >= x && mx <= (x + width) && my <= (y + height) && my >= y;
+        }
+    }
+
 }
