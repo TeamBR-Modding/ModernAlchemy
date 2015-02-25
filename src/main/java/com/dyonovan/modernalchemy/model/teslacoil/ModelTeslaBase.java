60728b90112a677e24cc68a4e5642dc98142eb4a
pauljoda <pauldavis101@gmail.com>
2015-02-25 11:14:12 -0600

    New Tesla Base

--- src/main/java/com/dyonovan/modernalchemy/model/teslacoil/ModelTeslaBase.java
+++ src/main/java/com/dyonovan/modernalchemy/model/teslacoil/ModelTeslaBase.java
@@ -11,21 +11,12 @@ public class ModelTeslaBase {
     public ModelTeslaBase() {
         modelBase = AdvancedModelLoader.loadModel(new ResourceLocation(Constants.MODID + ":models/teslaBase.obj"));
     }
-
-    //Model Names:
-    //Main   : NurbsPath.001_Mesh
-    //Switch : Cube.007_Cube.010
-    //Rotor  : Cylinder.014_Cylinder.018
-
+    
     public void renderMain() {
-        modelBase.renderPart("NurbsPath.001_Mesh");
-    }
-
-    public void renderSwitch() {
-        modelBase.renderPart("Cube.007_Cube.010");
+        modelBase.renderAllExcept("Rotor_Cylinder.019");
     }
 
     public void renderRotor() {
-        modelBase.renderPart("Cylinder.014_Cylinder.018");
+        modelBase.renderPart("Rotor_Cylinder.019");
     }
 }
