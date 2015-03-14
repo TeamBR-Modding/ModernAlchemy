730c3ceea9dfdfd21ebe22080749309dd586f34a
Paul Davis <pauldavis101@gmail.com>
2015-03-13 23:26:12 -0500

    Icons fix

--- src/main/java/com/dyonovan/modernalchemy/gui/GuiBaseConfigurableSlots.java
+++ src/main/java/com/dyonovan/modernalchemy/gui/GuiBaseConfigurableSlots.java
@@ -3,6 +3,7 @@ package com.dyonovan.modernalchemy.gui;
 import codechicken.nei.VisiblityData;
 import codechicken.nei.api.INEIGuiHandler;
 import codechicken.nei.api.TaggedInventoryArea;
+import com.dyonovan.teambrcore.TeamBRCore;
 import cpw.mods.fml.common.Optional;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.item.ItemStack;
@@ -13,17 +14,45 @@ import openmods.gui.component.GuiComponentTab;
 import openmods.gui.misc.IConfigurableGuiSlots;
 import openmods.sync.ISyncMapProvider;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Optional.InterfaceList({
         @Optional.Interface(iface = "codechicken.nei.api.INEIGuiHandler", modid = "NotEnoughItems")
 })
 public abstract class GuiBaseConfigurableSlots<T extends TileEntity&ISyncMapProvider&IConfigurableGuiSlots<E>, C extends ContainerBase<T>, E extends Enum<E>> extends GuiConfigurableSlots<T, C, E> implements INEIGuiHandler {
+    protected Rectangle arrowLoc;
+
     public GuiBaseConfigurableSlots(C container, int width, int height, String name) {
         super(container, width, height, name);
     }
 
+    public void setArrowLocation(int x, int y, int width, int height) {
+        arrowLoc = new Rectangle(x, y, width, height);
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float par3) {
+        super.drawScreen(mouseX, mouseY, par3);
+        if(arrowLoc != null && mouseX >= guiLeft + arrowLoc.x && mouseX <= guiLeft + arrowLoc.x + arrowLoc.width &&
+                mouseY >= guiTop + arrowLoc.y && mouseY <= guiTop + arrowLoc.y + arrowLoc.height &&
+                TeamBRCore.nei != null) {
+            drawHoveringText(Arrays.asList("Recipes"), mouseX, mouseY, fontRendererObj);
+        }
+    }
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int button)
+    {
+        if(arrowLoc != null && mouseX >= guiLeft + arrowLoc.x && mouseX <= guiLeft + arrowLoc.x + arrowLoc.width &&
+                mouseY >= guiTop + arrowLoc.y && mouseY <= guiTop + arrowLoc.y + arrowLoc.height &&
+                TeamBRCore.nei != null) {
+            TeamBRCore.nei.onArrowClicked(getContainer());
+        }
+        super.mouseClicked(mouseX, mouseY, button);
+    }
     public List<GuiComponentTab> tabs;
 
     @Override
